8383191a1cbaab3a3b3df524f41b666da018ec6f
thatguyislazy <marcadriancuano@gmail.com>
2026-06-02 00:28:55 +0800

    Updated: Fix FAQ's

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <FlowSection />
       <FeaturesSection />
       <PricingSection />
-      <FAQSection />
+      <FaqSection />
       {/* <StatsSection /> */}
       {/* <TestimonialsSection /> */}
       {/* <CtaSection /> */}
